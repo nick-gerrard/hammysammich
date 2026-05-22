65914f5524a37728c593ddfeafd36318127ca9c6
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-22 19:27:31 -0400

    add assets for doors and obstacles

--- map.lua
+++ map.lua
@@ -1,6 +1,14 @@
 local C = require("config")
 local Map = {}
 
+local obstacleImage
+local doorImage
+
+function Map.load()
+	obstacleImage = love.graphics.newImage("assets/obstacle.png")
+	doorImage = love.graphics.newImage("assets/door.png")
+end
+
 function Map.generateSolids(level)
 	local solids = {
 		{ x = 0, y = 0, width = C.VIRTUAL_W, height = C.WALL_THICKNESS, tag = C.OBJECT_TAGS.WALL },
@@ -30,11 +38,11 @@ end
 function Map.generateObstacles(size, level)
 	local obstacles = {}
 	local x_values = {}
-	for i = C.PLAY_MIN_X + size, C.PLAY_MAX_X - size * 2, size do
+	for i = C.PLAY_MIN_X + size * 2, C.PLAY_MAX_X - size * 2, size do
 		table.insert(x_values, i)
 	end
 	local y_values = {}
-	for i = C.PLAY_MIN_Y + size, C.PLAY_MAX_Y - size * 2, size do
+	for i = C.PLAY_MIN_Y + size * 2, C.PLAY_MAX_Y - size * 2, size do
 		table.insert(y_values, i)
 	end
 	for i = 1, level do
@@ -61,9 +69,18 @@ end
 function Map.draw(solids)
 	for i, solid in ipairs(solids) do
 		if solid.tag == C.OBJECT_TAGS.WALL then
+			love.graphics.setColor(1, 1, 1)
 			love.graphics.rectangle("line", solid.x, solid.y, solid.width, solid.height)
-		else
-			love.graphics.rectangle("fill", solid.x, solid.y, solid.width, solid.height)
+		elseif solid.tag == C.OBJECT_TAGS.OBSTACLE then
+			love.graphics.setColor(1, 1, 1)
+			local sx = solid.width / obstacleImage:getWidth()
+			local sy = solid.height / obstacleImage:getHeight()
+			love.graphics.draw(obstacleImage, solid.x, solid.y, 0, sx, sy)
+		elseif solid.tag == C.OBJECT_TAGS.DOOR then
+			love.graphics.setColor(1, 1, 1)
+			local sx = solid.width / doorImage:getWidth()
+			local sy = solid.height / doorImage:getHeight()
+			love.graphics.draw(doorImage, solid.x, solid.y, 0, sx, sy)
 		end
 	end
 end
